f0df0b41569f32a1c4a077b3050ff89ba108e930
Kai Mallea <kmallea@gmail.com>
2013-07-23 17:18:33 -0400

    Reformat Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,40 +1,39 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    jasmine: {
-      stopClock: {
-        src: ['stopclock.js'],
-        options: {
-          specs:   'tests/spec/*.js',
-          version: '1.3.1'
+    grunt.initConfig({
+        jasmine: {
+            stopclock: {
+                src: ['stopclock.js'],
+                options: {
+                    specs:   'tests/spec/*.js',
+                    version: '1.3.1'
+                }
+            },
+
+            stopclockmin: {
+                src: ['stopclock.min.js'],
+                options: {
+                    specs:   'tests/spec/*.js',
+                    version: '1.3.1'
+                }
+            }
+        },
+
+        uglify: {
+            min: {
+                files: {
+                    'stopclock.min.js': ['stopclock.js']
+                }
+            }
+        },
+
+        jshint: {
+            all: ['Gruntfile.js', 'stopclock.js', 'tests/spec/**/*.js']
         }
-      },
-      stopClockMin: {
-        src: ['stopclock.min.js'],
-        options: {
-          specs:   'tests/spec/*.js',
-          version: '1.3.1'
-        }
-      }
-    },
+    });
 
-    uglify: {
-      my_target: {
-        files: {
-          'stopclock.min.js': ['stopclock.js']
-        }
-      }
-    },
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    jshint: {
-      all: ['Gruntfile.js', 'stopclock.js', 'tests/spec/**/*.js']
-    }
-
-  });
-
-  grunt.loadNpmTasks('grunt-contrib-jasmine');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-
-  grunt.registerTask('default', ['jshint', 'uglify', 'jasmine']);
-
+    grunt.registerTask('default', ['jshint', 'uglify', 'jasmine']);
 };
